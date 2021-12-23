0635a7c0e816eb5acf51e0eb1c644bb8a1a97904
Max <max.edison@yahoo.com>
2021-12-23 13:34:20 -0500

    commit tree 1ed57ae91082e31737902629c1ed778e8e705f98

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+# rescue_from ActiveRecord::RecordInvalid, with: :render_the_unprocessable
+
+# private
+
+# def render_the_unprocessable
+# render json: {error: invalid.record.error.full_messages}, status: :unprocessable_entity
+# end
+
 end
